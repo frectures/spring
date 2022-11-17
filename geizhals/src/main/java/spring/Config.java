58a030ceba421a96c33670b09e34f290099c54b0
frectures <frectures@users.noreply.github.com>
2022-11-17 08:52:18 +0100

    replace WebMvcConfigurerAdapter with WebMvcConfigurer

--- geizhals/src/main/java/spring/Config.java
+++ geizhals/src/main/java/spring/Config.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "geizhals", "persistence" })
-public class Config extends WebMvcConfigurerAdapter {
+public class Config implements WebMvcConfigurer {
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		registry.jsp("/WEB-INF/jsp/", ".jsp");
